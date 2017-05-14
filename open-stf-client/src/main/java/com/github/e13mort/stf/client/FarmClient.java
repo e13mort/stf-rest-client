4c7154dd5eb212a426625d2d7e4e540ba5f5a902
Pavel <imortan@gmail.com>
2017-05-14 17:46:58 +0300

    Disconnect from currently connected devices

--- open-stf-client/src/main/java/com/github/e13mort/stf/client/FarmClient.java
+++ open-stf-client/src/main/java/com/github/e13mort/stf/client/FarmClient.java
@@ -14,6 +14,7 @@ import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
 import org.reactivestreams.Publisher;
 
+@SuppressWarnings("unused")
 public class FarmClient {
     private static final int DEFAULT_CONNECTION_TIMEOUT_SEC = 60;
     private final RxFarm rxFarm;
@@ -43,6 +44,22 @@ public class FarmClient {
                     @Override
                     public Publisher<String> apply(@NonNull Device device) throws Exception {
                         return rxFarm.connect(device.getSerial(), connectionTimeoutSec * 1000).toFlowable();
+                    }
+                })
+                .flatMap(new Function<String, Publisher<Notification<String>>>() {
+                    @Override
+                    public Publisher<Notification<String>> apply(@NonNull String s) throws Exception {
+                        return Flowable.just(Notification.createOnNext(s));
+                    }
+                });
+    }
+
+    public Flowable<Notification<String>> disconnectFromAllDevices() {
+        return rxFarm.getConnectedDevices()
+                .flatMap(new Function<Device, Publisher<String>>() {
+                    @Override
+                    public Publisher<String> apply(@NonNull Device device) throws Exception {
+                        return rxFarm.disconnect(device.getSerial()).toFlowable();
                     }
                 })
                 .flatMap(new Function<String, Publisher<Notification<String>>>() {
--- open-stf-rest-api-rxjava2-adapter/src/main/java/com/github/e13mort/stf/adapter/RxFarm.java
+++ open-stf-rest-api-rxjava2-adapter/src/main/java/com/github/e13mort/stf/adapter/RxFarm.java
@@ -9,13 +9,17 @@ import com.github.e13mort.stf.model.RemoteConnectUserDeviceResponse;
 import com.github.e13mort.stf.model.device.Device;
 import io.reactivex.*;
 import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.BiConsumer;
 import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.internal.operators.maybe.MaybeError;
 import retrofit2.Response;
 
+import java.io.IOException;
 import java.util.List;
 
 public class RxFarm {
-    public static final String DEVICES_FIELDS = "";
+    private static final String DEVICES_FIELDS = "";
     private final DevicesApi devicesApi;
     private final UserApi userApi;
 
@@ -45,43 +49,102 @@ public class RxFarm {
                 Response<DeviceListResponse> response = devicesApi
                         .getDevices(DEVICES_FIELDS)
                         .execute();
-                if (!response.isSuccessful()) {
-                    emitter.onError(new Exception(response.errorBody().string()));
-                } else {
-                    List<Device> devices = response.body().getDevices();
-                    for (Device device : devices) {
-                        if (!emitter.isCancelled()) {
-                            emitter.onNext(device);
-                        }
-                    }
-                    emitter.onComplete();
-                }
+                wrapDevices(emitter, response);
             }
         }, BackpressureStrategy.BUFFER);
     }
 
-    public Single<String> connect(final String serial, int timeout) {
-        if (serial == null) {
-            return Single.error(new NullPointerException("Serial is null"));
-        }
-        return addUserToDevice(serial, timeout).map(new DeviceConnectFunction());
+    public Flowable<Device> getConnectedDevices() {
+        return Flowable.create(new FlowableOnSubscribe<Device>() {
+            @Override
+            public void subscribe(FlowableEmitter<Device> e) throws Exception {
+                Response<DeviceListResponse> response = userApi
+                        .getUserDevices(DEVICES_FIELDS)
+                        .execute();
+                wrapDevices(e, response);
+            }
+        }, BackpressureStrategy.BUFFER);
     }
 
-    private Single<String> addUserToDevice(final String serial, final int timeout) {
-        return Single.create(new SingleOnSubscribe<String>() {
+    public Single<String> connect(final String serial, final int timeout) {
+        return from(serial)
+                .doOnEvent(addUserToDevice(serial, timeout))
+                .map(connectToDevice());
+    }
+
+    public Single<String> disconnect(final String serial) {
+        return from(serial)
+                .doOnEvent(disconnectFromDevice())
+                .doOnEvent(deleteDevice());
+    }
+
+    private void wrapDevices(FlowableEmitter<Device> emitter, Response<DeviceListResponse> response) throws IOException {
+        if (!response.isSuccessful()) {
+            emitter.onError(new Exception(response.errorBody().string()));
+        } else {
+            List<Device> devices = response.body().getDevices();
+            for (Device device : devices) {
+                if (!emitter.isCancelled()) {
+                    emitter.onNext(device);
+                }
+            }
+            emitter.onComplete();
+        }
+    }
+
+    private BiConsumer<String, Throwable> deleteDevice() {
+        return new BiConsumer<String, Throwable>() {
             @Override
-            public void subscribe(SingleEmitter<String> e) throws Exception {
+            public void accept(@NonNull String s, @NonNull Throwable throwable) throws Exception {
+                Response<Object> response = userApi.deleteUserDeviceBySerial(s).execute();
+                if (!response.isSuccessful()) {
+                    throw new ResponseException(response.code(), response.message());
+                }
+            }
+        };
+    }
+
+    private BiConsumer<String, Throwable> disconnectFromDevice() {
+        return new BiConsumer<String, Throwable>() {
+            @Override
+            public void accept(@NonNull String s, @NonNull Throwable throwable) throws Exception {
+                Response<Object> response = userApi.remoteDisconnectUserDeviceBySerial(s).execute();
+                if (!response.isSuccessful()) {
+                    throw new ResponseException(response.code(), response.message());
+                }
+            }
+        };
+    }
+
+    private Single<String> from(String serial) {
+        return Single.just(serial)
+                .filter(new Predicate<String>() {
+                    @Override
+                    public boolean test(@NonNull String s) throws Exception {
+                        return s != null;
+                    }
+                })
+                .switchIfEmpty(new MaybeError<String>(new NullPointerException("Serial is null")))
+                .toSingle();
+    }
+
+    private DeviceConnectFunction connectToDevice() {
+        return new DeviceConnectFunction();
+    }
+
+    private BiConsumer<String, Throwable> addUserToDevice(final String serial, final int timeout) {
+        return new BiConsumer<String, Throwable>() {
+            @Override
+            public void accept(@NonNull String s, @NonNull Throwable throwable) throws Exception {
                 AddUserDevicePayload payload = new AddUserDevicePayload();
                 payload.setSerial(serial);
                 payload.setTimeout(timeout);
                 Response<Object> response = userApi.addUserDevice(payload).execute();
-                if (response.isSuccessful()) {
-                    e.onSuccess(serial);
-                } else {
-                    e.onError(new ResponseException(response));
+                if (!response.isSuccessful()) {
+                    throw new ResponseException(response);
                 }
             }
-        });
+        };
     }
 
     public class DeviceConnectFunction implements Function<String, String> {
