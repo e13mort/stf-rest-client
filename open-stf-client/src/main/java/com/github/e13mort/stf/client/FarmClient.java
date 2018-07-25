3269667a9a5cd3c612f4c4637b01687894824dcb
Pavel Novikov <imortan@gmail.com>
2018-07-26 00:52:15 +0300

    FarmClient: new disconnect by serial numbers method added and tested. Existing connect/disconnect methods are fixed.

--- open-stf-client/src/main/java/com/github/e13mort/stf/client/FarmClient.java
+++ open-stf-client/src/main/java/com/github/e13mort/stf/client/FarmClient.java
@@ -9,15 +9,21 @@ import com.github.e13mort.stf.client.parameters.DevicesParams;
 import com.github.e13mort.stf.model.device.Device;
 import io.reactivex.Flowable;
 import io.reactivex.Notification;
-import io.reactivex.annotations.NonNull;
+import io.reactivex.Single;
 import io.reactivex.functions.Function;
-import org.reactivestreams.Publisher;
+
+import java.util.List;
 
 @SuppressWarnings({"unused", "WeakerAccess"})
 public class FarmClient {
     private static final int DEFAULT_CONNECTION_TIMEOUT_SEC = 60;
     private final RxFarm rxFarm;
     private final int connectionTimeoutSec;
+    private final Function<Single<String>, Flowable<Notification<String>>> notificationConversionFunction =
+            stringSingle -> stringSingle
+                    .toFlowable()
+                    .map(Notification::createOnNext)
+                    .onErrorReturn(Notification::createOnError);
 
     public static FarmClient create(FarmInfo farmInfo) {
         if (farmInfo == null) {
@@ -43,14 +49,17 @@ public class FarmClient {
 
     public Flowable<Notification<String>> connectToDevices(DevicesParams params) {
         return getDevices(params)
-                .flatMap(device -> rxFarm.connect(device.getSerial(), connectionTimeoutSec * 1000).toFlowable())
-                .flatMap(s -> Flowable.just(Notification.createOnNext(s)));
+                .flatMap(device -> rxFarm.connect(device.getSerial(), connectionTimeoutSec * 1000).to(notificationConversionFunction));
     }
 
     public Flowable<Notification<String>> disconnectFromAllDevices() {
         return rxFarm.getConnectedDevices()
-                .flatMap(device -> rxFarm.disconnect(device.getSerial()).toFlowable())
-                .flatMap(s -> Flowable.just(Notification.createOnNext(s)));
+                .flatMap(device -> rxFarm.disconnect(device.getSerial()).to(notificationConversionFunction));
+    }
+
+    public Flowable<Notification<String>> disconnectFromDevices(List<String> serialNumbers) {
+        return Flowable.fromIterable(serialNumbers)
+                .flatMap(serial -> rxFarm.disconnect(serial).to(notificationConversionFunction));
     }
 
 }
